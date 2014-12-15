b83f5c4e9e60ab5ff85b599d95cee180a12c7903
Andy Lowry <andrew.lowry@laterooms.com>
2014-12-15 17:32:51 +0000

    Merge Farz's changes which for some reason he never committed

--- laterooms/assets/js/index.js
+++ laterooms/assets/js/index.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 
 	$('pre').addClass('prettyprint');
-	
+	coverImages();
+});
+
+var coverImages = function coverImages() {
 	var mainImage = $('img[alt="main-image"]');
-	
+
 	if ( mainImage.length > 0){
 		mainImage.remove();
 		var mainImageSource = mainImage.attr('src');
@@ -11,6 +14,5 @@ $(document).ready(function(){
 		$('#post-header').addClass('active');
 	} else {
 		$('#post-header').addClass('active');
-	}
-
-});
+	}	
+}
