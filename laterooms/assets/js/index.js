51a10cbb30c3bd844a71af9f52373ade3190aef0
Steve Elliott <steve@tegud.net>
2015-12-06 23:15:57 +0000

    Fix styles outside of paragraphs, introduce "purple bar"

--- laterooms/assets/js/index.js
+++ laterooms/assets/js/index.js
@@ -5,14 +5,5 @@ $(document).ready(function(){
 });
 
 var coverImages = function coverImages() {
-	var mainImage = $('img[alt="main-image"]');
-
-	if ( mainImage.length > 0){
-		mainImage.remove();
-		var mainImageSource = mainImage.attr('src');
-		$('#post-header').css('background-image','url('+mainImageSource+')');
-		$('#post-header').addClass('active');
-	} else {
-		$('#post-header').addClass('active');
-	}	
+	$('#post-header').addClass('active');
 }
